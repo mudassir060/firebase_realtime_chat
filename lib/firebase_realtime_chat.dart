54db5029d1e96cdbca51ed0cbecb53d7aad79461
mudassir060 <mudassirmukhtar4@gmail.com>
2024-05-08 18:00:10 +0500

    create app

---
+++ lib/firebase_realtime_chat.dart
@@ -0,0 +1,9 @@
+library firebase_realtime_chat;
+
+// export '/date_time_piker/time_piker.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
